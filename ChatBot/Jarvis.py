0bf4e8ba2f38630305863a5418d8da8062cf5881
rog01 <45034536+rog01@users.noreply.github.com>
2021-02-23 16:06:31 +0100

    Add files via upload

--- ChatBot/Jarvis.py
+++ ChatBot/Jarvis.py
@@ -11,13 +11,12 @@ import chatter_bot as ct
 import pickle
 
 
-topics = ['data_sciences', 'artificial_intelligence', 'travel','music','movies','english']
+topics = ['data_sciences', 'artificial_intelligence', 'quantum_computing','travel','music','movies']
 
 client = discord.Client()
 
 found = False
-channel_closed = []
-channel_comment_saved = []
+comment_flag = False
 
 with open('/home/roger/anaconda3/projetIA/Chat_bot/ChatBot/topic_classifier.pkl','rb') as file:
     topic_clf = pickle.load(file)
@@ -28,7 +27,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global topic, lan, found, channel_closed, channel_comment_saved
+    global topic, lan, found, comment_flag
     #traduction message user
     if message.content not in ['/help','/h']:
         ms,lan = preproc.transin(message.content)
@@ -38,7 +37,7 @@ async def on_message(message):
     
     if message.author != client.user:
         #Gestion de la conversation
-        if msg in library.lib and message.channel.name not in channel_closed :
+        if msg in library.lib and message.channel.name not in channel_closed and comment_flag == False:
             print(message.channel)
             if msg in ['hello'] or message.content in ['/help','/h']:       
                 await message.channel.send(file=discord.File('/home/roger/anaconda3/projetIA/Chat_bot/ChatBot/robot.png'))
@@ -49,7 +48,7 @@ async def on_message(message):
         else:
             #Gestion de la recherche en bdd   
             try:
-                if msg not in library.lib and preproc.transout(msg,'en') != 'no' and preproc.transout(msg,'en') != 'yes' and message.channel.name not in channel_closed: 
+                if msg not in library.lib and preproc.transout(msg,'en') != 'no' and comment_flag == False and preproc.transout(msg,'en') != 'yes' : 
                     topic = topic_clf.predict([msg])[0]
                     print("search",topic_clf.predict([msg])[0])
                     resp = search.find_question_answer(msg,topic)
@@ -57,21 +56,24 @@ async def on_message(message):
                     resp = preproc.transout(resp,lan)
                     await message.channel.send(resp[:2000])
             except NameError:
-                if  preproc.transout(msg,'en') != 'no' and message.channel.name not in channel_closed:
+                if  preproc.transout(msg,'en') != 'no' and comment_flag == False:
                     response = preproc.transout('Pouvez-vous m\'en dire plus ?',lan)
                     await message.channel.send(response)                        
         # fin de conversation
-        if found == True and msg not in library.lib and preproc.transout(msg,'en') != 'no' and preproc.transout(msg,'en') != 'yes'and message.channel.name not in channel_closed:
+        if found == True and msg not in library.lib and preproc.transout(msg,'en') != 'no' and preproc.transout(msg,'en') != 'yes'and comment_flag == False:
             resp = preproc.transout(library.end_of_conv[0], lan)+library.end_of_conv[1]
             await message.channel.send(resp)
-        if preproc.transout(msg,'en') == 'no':
+        if preproc.transout(msg,'en') == 'no' and comment_flag == False:
             resp = preproc.transout(library.bot_end_conv[0],lan)
             await message.channel.send(resp)
-            channel_closed.append(message.channel.name)
-        if preproc.transout(msg,'en') != 'no' and message.channel.name in channel_closed and message.channel.name not in channel_comment_saved:
+            comment_flag = True
+            #channel_closed.append(message.channel.name)
+        if preproc.transout(msg,'en') != 'no' and comment_flag == True :
             feed_back.add_feeback({"message":message.content,
                                     "date":str(datetime.date.today())})
-            channel_comment_saved.append(message.channel.name)
+            #channel_comment_saved.append(message.channel.name)
+            comment_flag = False
+
 
 token = ''
 client.run(token)
